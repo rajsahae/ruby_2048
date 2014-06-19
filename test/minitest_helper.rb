a77aeab185adc1a795bd59fddf8c98c8bde577d9
Raj Sahae <rsahae@teslamotors.com>
2014-06-19 00:22:53 -0700

    Needed to reorder the minitest requires to stop an error

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ruby_2048'
 
-require 'minitest/spec'
 require 'minitest/autorun'
+require 'minitest/spec'
 require 'minitest/pride'
