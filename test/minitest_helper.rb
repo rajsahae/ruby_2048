f79871e1f8d521ae21a0fb5d38b27fa0dcb18a96
Raj Sahae <rsahae@teslamotors.com>
2014-06-19 13:35:21 -0700

    Changed RACK_ENV variable. Also changed Ruby2048 test to use spec.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+ENV['RACK_ENV'] = 'test'
+
 require 'ruby_2048'
+require 'ruby_2048/server'
 
 require 'minitest/autorun'
 require 'minitest/spec'
--- test/test_ruby_2048.rb
+++ test/test_ruby_2048.rb
@@ -3,8 +3,8 @@
 
 require 'minitest_helper'
 
-class TestRuby2048 < MiniTest::Unit::TestCase
-  def test_that_it_has_a_version_number
+describe Ruby2048 do
+  it "has a version number" do
     refute_nil ::Ruby2048::VERSION
   end
 end
