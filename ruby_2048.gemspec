7fcab6b4983d87a2d1fecbb588a65fec14e68cb7
Raj Sahae <rsahae@teslamotors.com>
2014-06-19 15:32:32 -0700

    Had to run bundle install and change the gemfile to get rack/test working

--- ruby_2048.gemspec
+++ ruby_2048.gemspec
@@ -23,6 +23,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'minitest'
   spec.add_development_dependency 'shotgun'
+  spec.add_development_dependency 'rack-test'
   spec.add_dependency 'sinatra'
   spec.add_dependency 'markaby'
 end
---
+++ test/test_ruby_2048_server.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require 'minitest_helper'
+require 'rack/test'
+
+module Ruby2048
+
+  describe "Server" do
+    include Rack::Test::Methods
+
+    def app
+      Sinatra::Application
+    end
+
+    it "should return the id for json encoded games" do
+      rfc4122 = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+      get '/create?encoding=json'
+      last_response.ok?.must_equal(true)
+      last_response.body.must_match(rfc4122)
+    end
+
+    it "should redirect to /play/id for regular games" do
+      get '/create'
+      last_response.ok?.must_equal(true)
+      p last_response
+    end
+  end
+
+end
